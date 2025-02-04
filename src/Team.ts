5577bd4c9b07f6ba47ab1d7b68ca4c8813da883e
Dharmik Gohil <gohildharmik2020@gmail.com>
2025-02-04 12:06:57 +0530

    added testing code for edge cases for team

--- src/Team.ts
+++ src/Team.ts
@@ -1,13 +1,19 @@
 import { checkForNegative } from "../helper/error";
 import { Player } from "./Player";
-
+const TEAM_RULES = {
+    MAX_BATSMAN: 5,
+    MAX_BOWLER: 5,
+    MAX_WICKETKEEPER: 1,
+    MAX_PLAYERS: 11,
+    TOTAL_CREDITS: 100,
+}
 export class Team { 
     private team : Player[];
     private name : string;
     private fantasyPoints : number = 0;
     private runs : number = 0;
     private isTossWinner : boolean = false;
-    private credits : number = 100;
+    private credits : number = TEAM_RULES.TOTAL_CREDITS;
     private batsmanCount : number = 0;
     private bowlerCount : number = 0;
     private wicketKeeperCount : number = 0;
@@ -58,8 +64,28 @@ export class Team {
         this.isTossWinner = true;
     }
     addPlayer(player: Player) { 
+        if(this.team.includes(player)) {
+            throw new Error('Player already added');
+        }
+        if (this.team.length >= TEAM_RULES.MAX_PLAYERS) {
+            throw new Error('Maximum team size reached');
+        } 
+        if (this.credits - player.getCredit() < 0) {
+            throw new Error('Not enough credits');
+        }
+        if(player.getRole() === "Batsman" && this.batsmanCount >= TEAM_RULES.MAX_BATSMAN) {
+            throw new Error('Maximum Batsman limit reached');
+        }
+        if(player.getRole() === "Bowler" && this.bowlerCount >= TEAM_RULES.MAX_BOWLER) {
+            throw new Error('Maximum Bowler limit reached');
+        }
+        if(player.getRole() === "Wicketkeeper" && this.wicketKeeperCount >= TEAM_RULES.MAX_WICKETKEEPER) {
+            throw new Error('Maximum Wicketkeeper limit reached');
+        }
+
         this.team.push(player);
         this.decreaseCredits(player.getCredit());
+
         if(player.getRole() === "Batsman") {
             this.increaseBatsmanCount();
         }
--- test/Team.test.ts
+++ test/Team.test.ts
@@ -1,21 +1,27 @@
 import { Player } from '../src/Player';
 import { Team } from '../src/Team';
 
+const createPlayer = (name: string, role: string, credits: number) => {
+    return new Player(name, role, credits, false, false, 0, 0, false, 0, "", 0);
+}
+
+
 describe("Initial state", () => {
     test("it should be start with empty team", () => {
-        const team = new Team("Chennai Super Kings");
+        const team = new Team("CSK");
         expect(team.getTeam().length).toBe(0);
     })
 })
 
-describe("After adding players", () => {
+describe("Basic team operations", () => {
     let team : Team;
     beforeEach(() => {
-        team = new Team("Royal Challengers Bangalore");
+        team = new Team("RCB");
         team.addPlayer(new Player("Virat Kohli", "Batsman", 10, false, false, 0, 0, false, 0, "", 0));
         team.addPlayer(new Player("Jasprit Bumrah", "Bowler", 12, false, false, 0, 0, false, 0, "", 0));
         team.addPlayer(new Player("Rohit Sharma", "Batsman", 11, false, false, 0, 0, false, 0, "", 0));
-        team.addPlayer(new Player("MS Dhoni", "Wicketkeeper", 10, false, false, 0, 0, false, 0, "", 0));
+        team.addPlayer(new Player("Dinesh Karthik", "Wicketkeeper", 10, false, false, 0, 0, false, 0, "", 0));
+        
     })
     test("team name should be defined", () => {
         expect(team.getName()).toBeDefined();
@@ -50,13 +56,65 @@ describe("After adding players", () => {
         team.addRuns(10);
         expect(team.getRuns()).toBe(10);
     })
+})
 
-    // // checking for negative values
+describe("Testing edge cases", () => {
+    let team : Team | null;
+    beforeEach(() => {
+        team = new Team("RCB");
+    })
+    afterEach(() => {
+        team = null;
+    })
+    // adding same player twice
+    test("team should throw error if same player added twice", () => {
+        const player = new Player("Virat Kohli", "Batsman", 10, false, false, 0, 0, false, 0, "", 0);
+        team?.addPlayer(player);
+        expect(() => team?.addPlayer(player)).toThrowError("Player already added");  
+    })
+    
+    // adding more than 5 batsmen
+    test("team should throw error if more than 5 batsmen are add", () => {
+        const batsmen = Array.from({length : 5}, (_, i) => new Player(`Batsman ${i}`, "Batsman", 10, false, false, 0, 0, false, 0, "", 0));
+        batsmen.forEach(batsman => team?.addPlayer(batsman));
+        expect(() => team?.addPlayer(new Player("Ravindra Jadeja", "Batsman", 10, false, false, 0, 0, false, 0, "", 0))).toThrowError("Maximum Batsman limit reached");
+    })
+ 
+    // adding more than 5 bowlers
+    test("team should throw error if more than 5 bowlers are add", () => {
+        const bowlers = Array.from({length : 5}, (_, i) => new Player(`Bowler ${i}`, "Bowler", 10, false, false, 0, 0, false, 0, "", 0));
+        bowlers.forEach(bowler => team?.addPlayer(bowler));
+        expect(() => team?.addPlayer(new Player("Ravichandran Ashwin", "Bowler", 11, false, false, 0, 0, false, 0, "", 0))).toThrowError("Maximum Bowler limit reached");
+    })
+
+    // adding more than 1 wicketkeeper
+    test("team should throw error if more than 1 wicketkeeper is added", () => {
+        team?.addPlayer(new Player("Dinesh Karthik", "Wicketkeeper", 10, false, false, 0, 0, false, 0, "", 0));
+        expect(() => team?.addPlayer(new Player("KL Rahul", "Wicketkeeper", 10, false, false, 0, 0, false, 0, "", 0))).toThrowError("Maximum Wicketkeeper limit reached");
+    })
+    
+    // adding more than 11 players
+    test("team should throw error if more than 11 players are added", () => {
+        const players = [
+            ...Array.from({length : 5}, (_, i) => new Player(`Batsman ${i}`, "Batsman", 5, false, false, 0, 0, false, 0, "", 0)),
+            ...Array.from({length : 5}, (_, i) => new Player(`Bowler ${i}`, "Bowler", 5, false, false, 0, 0, false, 0, "", 0)),
+            new Player("Dinesh Karthik", "Wicketkeeper", 10, false, false, 0, 0, false, 0, "", 0)
+        ]
+        players.forEach(player => team?.addPlayer(player));
+        expect(() => team?.addPlayer(new Player("KL Rahul", "Wicketkeeper", 10, false, false, 0, 0, false, 0, "", 0))).toThrowError("Maximum team size reached");
+    })
+    // checking for negative values 
     test("team should throw error if negative fantasy points added", () => {
-        expect(() => team.addFantasyPoints(-10)).toThrowError("Fantasy Points cannot be negative");
+        expect(() => team?.addFantasyPoints(-10)).toThrowError("Fantasy Points cannot be negative");
     })
 
     test("team should throw error if negative runs added", () => {
-        expect(() => team.addRuns(-10)).toThrowError("Runs cannot be negative");
+        expect(() => team?.addRuns(-10)).toThrowError("Runs cannot be negative");
+    })
+    // checking for enough credits
+    test("team should throw error if not enough credits", () => {
+        const player = new Player("Virat Kohli", "Batsman", 100, false, false, 0, 0, false, 0, "", 0);
+        team?.addPlayer(player);
+        expect(() => team?.addPlayer(new Player("Jasprit Bumrah", "Bowler", 12, false, false, 0, 0, false, 0, "", 0))).toThrowError("Not enough credits");   
     })
 })
